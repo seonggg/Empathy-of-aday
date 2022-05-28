145957e8fc84c4ec3bb342d1b488cb84cbc7bbc4
rse <seongeun7877@naver.com>
2022-05-28 17:05:42 +0900

    DB 연동 이미지 저장 및 출력(writer_id 빼고 구현)

--- app/src/main/java/com/project2022/emotiondiary/DiaryWrite.java
+++ app/src/main/java/com/project2022/emotiondiary/DiaryWrite.java
@@ -7,7 +7,11 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -21,6 +25,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,8 +47,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-import io.grpc.Context;
-
 public class DiaryWrite extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -52,18 +55,18 @@ public class DiaryWrite extends AppCompatActivity {
 
     private final String TAG = this.getClass().getSimpleName();
 
-    private final int GALLEY_CODE=10;
-
     ImageView imageview;
     Button completeBtn;
     ImageButton weatherB;
     EditText editText;
 
+    LinearLayout recyclerCase;
+    private ViewPager2 sliderViewPager;
+
     String weather="맑음";
     String docid;
 
-    ArrayList<Uri> imageArray;
-    Uri uri;
+    ArrayList<Uri> uriList= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,16 +88,22 @@ public class DiaryWrite extends AppCompatActivity {
 
         //이미지 업로드
         imageview = findViewById(R.id.photoView);
+        recyclerCase = findViewById(R.id.recyclerCase);
+        sliderViewPager=findViewById(R.id.sliderViewPager);
 
-        imageview.setOnClickListener(v -> {
 
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.setAction(Intent.ACTION_GET_CONTENT);
+        //imageview.setOnClickListener(v -> {
+        recyclerCase.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            imageview.setVisibility(View.GONE);
+            sliderViewPager.setVisibility(View.VISIBLE);
             launcher.launch(intent);
 
-
-            //loadAlbum();
         });
 
         //작성완료 버튼 이벤트
@@ -121,7 +130,9 @@ public class DiaryWrite extends AppCompatActivity {
                                 Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
                                 //docid = documentReference.getPath();
                                 docid = documentReference.getId();
-                                uploadImg(uri);
+                                for(int i=0;i<=uriList.size()-1;i++) {
+                                    uploadImg(uriList.get(i),i);
+                                }
                                 // 감정 분석 화면으로 전환
                                 Intent intent = new Intent(getApplicationContext(), BeadsMaking.class);
                                 intent.putExtra("docid", docid);
@@ -148,13 +159,44 @@ public class DiaryWrite extends AppCompatActivity {
                 {
                     if (result.getResultCode() == RESULT_OK)
                     {
-                        Log.e(TAG, "result : " + result);
                         Intent intent = result.getData();
-                        Log.e(TAG, "intent : " + intent);
-                        assert intent != null;
-                        uri = intent.getData();
-                        imageview.setImageURI(uri);
-                        //imageArray.add(uri);
+
+                        if(intent == null){   // 어떤 이미지도 선택하지 않은 경우
+                            Toast.makeText(getApplicationContext(), "이미지를 선택하지 않았습니다.", Toast.LENGTH_LONG).show();
+                        }
+                        else{   // 이미지를 하나라도 선택한 경우
+                            if(intent.getClipData() == null){     // 이미지를 하나만 선택한 경우
+                                Log.e("single choice: ", String.valueOf(intent.getData()));
+                                Uri imageUri = intent.getData();
+                                uriList.add(imageUri);
+                            }
+                            else{      // 이미지를 여러장 선택한 경우
+                                ClipData clipData = intent.getClipData();
+                                Log.e("clipData", String.valueOf(clipData.getItemCount()));
+
+                                if(clipData.getItemCount() > 5){   // 선택한 이미지가 5장 이상인 경우
+                                    Toast.makeText(getApplicationContext(), "사진은 5장까지 선택 가능합니다.", Toast.LENGTH_LONG).show();
+                                }
+                                else{   // 선택한 이미지가 5장 이하인 경우
+                                    Log.e(TAG, "multiple choice");
+
+                                    for (int i = 0; i < clipData.getItemCount(); i++){
+                                        Uri imageUri = clipData.getItemAt(i).getUri();  // 선택한 이미지들의 uri를 가져온다.
+                                        try {
+                                            uriList.add(imageUri);  //uri를 list에 담는다.
+
+                                        } catch (Exception e) {
+                                            Log.e(TAG, "File select error", e);
+                                        }
+                                    }
+                                }
+                            }
+
+                            //선택한 이미지 viewpager2에 출력
+                            sliderViewPager.setOffscreenPageLimit(1);
+                            sliderViewPager.setAdapter(new ImageSliderAdapter(getApplicationContext(), uriList));
+
+                        }
                     }
                 }
             });
@@ -192,9 +234,9 @@ public class DiaryWrite extends AppCompatActivity {
     }
 
     //storage에 사진 저장
-    private void uploadImg(Uri file){
+    private void uploadImg(Uri file, int num){
         StorageReference storageRef = storage.getReference();
-        StorageReference riversRef = storageRef.child("diary/"+docid+"1.jpg");
+        StorageReference riversRef = storageRef.child("diary/"+docid+"_"+num+".jpg");
         UploadTask uploadTask = riversRef.putFile(file);
 
             try{
