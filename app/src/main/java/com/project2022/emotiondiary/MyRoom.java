5389bb4daed05c4e818e8f50d3475b2e4f2a7c61
rse <seongeun7877@naver.com>
2022-06-05 21:48:01 +0900

    일기 보기 버튼 누르면 사용자(로그인한 아이디)의 가장 최신 글 불러오기

--- app/src/main/java/com/project2022/emotiondiary/MyRoom.java
+++ app/src/main/java/com/project2022/emotiondiary/MyRoom.java
@@ -3,6 +3,7 @@ package com.project2022.emotiondiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.Random;
 
 public class MyRoom extends AppCompatActivity {
 
